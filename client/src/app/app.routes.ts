9251df854c3924c09158dacc80f56420a2167daa
Geeth Sowri <nainalageethsowri@gmail.com>
2025-09-11 10:59:15 +0530

    Remove unused app component files and configurations, including HTML, SCSS, TypeScript, and related tests, to streamline the project structure.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { HelperformComponent } from './helperform/helperform.component';
 import { EditHelperComponent } from './edit-helper/edit-helper.component';
 
 export const routes: Routes = [
-    {path:'', redirectTo: '/helpers', pathMatch: 'full'},
+    { path: '', redirectTo: '/helpers', pathMatch: 'full' },
     { path: 'helpers', component: HelpersComponent },
-    { path: 'helpers/add-helper', component:HelperformComponent},
-    { path: 'helpers/edit-helper/:id', component:EditHelperComponent }
+    { path: 'helpers/add-helper', component: HelperformComponent },
+    { path: 'helpers/edit-helper/:id', component: EditHelperComponent }
 ];
